99ab9861bb8d6377fb47e60b375f36ddcb25656b
othsueh <ych930719@gmail.com>
2024-01-12 21:41:13 +0800

    23 update

--- lua/lush_theme/oth1.lua
+++ lua/lush_theme/oth1.lua
@@ -198,7 +198,7 @@ local theme = lush(function(injected_functions)
 		--
 		-- DiagnosticError            { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
 		DiagnosticWarn({ fg = hsl("#b9375e") }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		DiagnosticInfo({ fg = hsl("#ffffff") }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticInfo({ fg = hsl("#ff85ab") }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
 		-- DiagnosticHint             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
 		-- DiagnosticOk               { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
 		-- DiagnosticVirtualTextError { } , -- Used for "Error" diagnostic virtual text.
